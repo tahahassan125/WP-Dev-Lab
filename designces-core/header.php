87f88faedfb93f83bfd175261b4c35334965e72e
tahahassan125 <designces@outlook.com>
2026-09-09 17:16:35 +0500

    updated

--- designces-core/header.php
+++ designces-core/header.php
@@ -23,7 +23,11 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'designces-core' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary">
+	<?php esc_html_e( 'Skip to content', 'designces-core' ); ?>
+</a>
+<?php get_template_part( 'template-parts/header/announcement-bar' ); ?>
+
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
---
+++ designces-core/template-parts/header/announcement-bar.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Announcement bar.
+ *
+ * @package Designces_Core
+ */
+?>
+
+<div class="announcement-bar pt-2 pb-2">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-4">
+
+				<ul class="announcement-bar__list">
+					<li>
+						<i class="bi bi-telephone rounded-circle"></i>
+						<a href="tel:+44 555 22211" class="text-decoration-none">+44 555 22211</a>
+					</li>
+
+					<li>
+						<i class="bi bi-envelope rounded-circle"></i>
+						<a href="mailto:hello@pawsgang.shop" class="text-decoration-none">hello@pawsgang.shop</a>
+					</li>
+				</ul>
+
+			</div>
+
+			<div class="col-md-8 d-flex justify-content-end">
+
+				<ul class="announcement-bar__list">
+					<li>
+						<i class="bi bi-truck rounded-circle"></i>FREE EU SHIPPING
+					</li>
+
+					<li>
+						<i class="bi bi-clock-history rounded-circle"></i>30 DAYS MONEYBACK GUARANTEE
+					</li>
+
+					<li>
+						<i class="bi bi-person rounded-circle"></i>24/7 CUSTOMER SUPPORT
+					</li>
+				</ul>
+
+			</div>
+
+		</div>
+	</div>
+</div>
